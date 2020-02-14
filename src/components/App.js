b90de2727ac310c9822b526e1d46dd37f55f1ca7
garimabeli-dot <61051726+garimabeli-dot@users.noreply.github.com>
2020-02-14 21:48:23 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import { Button } from 'react-bootstrap';
 import Details from './Details';
+import '../styles/index.css';
 
 class App extends Component {
     constructor(props) {
         super(props);
-
+        
         this.state = {
             userJson: [],
+            isDataLoaded: false,
+            instructionText: "Click on Load to Get Details"
         }
     }
     loadDetails = () => {
@@ -16,18 +19,22 @@ class App extends Component {
             .then(response => response.json())
             .then(json => this.setState({
                 userJson: json.data,
+                isDataLoaded: true,
+                instructionText:"Click on Link Below to Load the Profile"
             })
             );
     }
     render() {
         return (
             <div className="App">
-                <h2>Welcome</h2>
-                <h5>Welcome to the world of details</h5>             
-                    <Details userDetails={this.state.userJson} /> 
+                <div className="justify-content-md-center"><h2>Welcome</h2></div>
+        <div className="header-detail"><h5>{this.state.instructionText}</h5></div>
+                {this.state.isDataLoaded ?
+                    <Details userDetails={this.state.userJson} /> : ""}
                 <div>
                     <Button
                         variant="outline-primary"
+                        className="btn"
                         onClick={this.loadDetails}>
                         LOAD DETAILS
                     </Button>
